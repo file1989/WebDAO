fa8a389a3778e6c12f1bfc19f5f5e3caa4b9e78b
file1989 <file1989@126.com>
2016-11-09 10:55:14 +0800

    lzp创建

---
+++ App_Code/JSON.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// JSON 帮助类
+/// </summary>
+public class JSON
+{
+	public JSON(){}
+
+}
---
+++ App_Code/SQLiteDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// SQLiteDAO 的摘要说明
+/// </summary>
+public class SQLiteDAO
+{
+	public SQLiteDAO()
+	{
+		//
+		// TODO: 在此处添加构造函数逻辑
+		//
+	}
+}
