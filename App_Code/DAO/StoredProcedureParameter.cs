218cfdafab34ce752c62e3eb2b83fec800fe8eaf
file1989 <file1989@126.com>
2016-11-13 11:41:56 +0800

    lzp 201611131141

---
+++ App_Code/DAO/StoredProcedureParameter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// 存储过程参数
+/// </summary>
+public class StoredProcedureParameter
+{
+    /// <summary>
+    /// 名称
+    /// </summary>
+    public string name;
+    /// <summary>
+    /// 类型
+    /// </summary>
+    public string type;
+    /// <summary>
+    /// 大小
+    /// </summary>
+    public int length;
+    /// <summary>
+    /// 是否输出参数
+    /// </summary>
+    public bool isoutparam;
+    /// <summary>
+    /// 是否可以为空
+    /// </summary>
+    public bool isnullable;
+
+}
+
+/// <summary>
+/// 
+/// </summary>
+public class StoredProcedureParameterFactory {
+    private static Dictionary<string, List<StoredProcedureParameter>> StoredProcedureParameterPool = new Dictionary<string, List<StoredProcedureParameter>>();
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public static bool Clear()
+    {
+        try
+        {
+            StoredProcedureParameterPool.Clear();
+            return StoredProcedureParameterPool.Count==0;
+        }
+        catch { return false; }
+    }
+
+
+    public List<StoredProcedureParameter> this[string ProcedureName]
+    {
+        get
+        {
+            if (StoredProcedureParameterPool.ContainsKey(ProcedureName))
+            {
+                return StoredProcedureParameterPool[ProcedureName];
+            }
+            else { return null; }
+        }
+        set
+        {
+            if (StoredProcedureParameterPool.ContainsKey(ProcedureName))
+            {
+                StoredProcedureParameterPool[ProcedureName] = value;
+            }
+            else
+            {
+                StoredProcedureParameterPool.Add(ProcedureName, value);
+            }
+        }
+    }
+
+}
---
+++ test/Handler.ashx
@@ -0,0 +1,29 @@
+﻿<%@ WebHandler Language="C#" Class="Handler" %>
+
+using System;
+using System.Web;
+using System.Drawing;
+using System.IO;
+public class Handler : IHttpHandler {
+    public bool IsReusable { get { return false; } }
+    public void ProcessRequest (HttpContext context) {
+        context.Response.ContentType = "text/html";
+        TestToBase64(context);
+        
+    }
+ 
+
+    public void TestToBase64(HttpContext context)
+    {
+        
+        Image img = Image.FromStream(context.Request.Files[0].InputStream);
+        string t = Common.ToBase64(img);
+        context.Response.Write("<img src=\"data:img/jpg;base64,"+t+ "\"/>" );
+        Common.ToImage(t).Save("d:/11/11.jpg");
+        Common.GetImageThumbnail(img, 360, 400, 10).Save("d:/11/Thumbnail-11.Jpeg");
+        Common.ImageCompress("d:/11/11.jpg", "d:/11/ImageCompress-l.jpg", 4160, 3120, 80);
+
+        
+    }
+
+}
