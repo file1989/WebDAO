39e615295703e390906b2a05de1b460626518cf0
file1989 <file1989@126.com>
2017-01-06 11:28:40 +0800

    20170106

---
+++ asmx_test.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ServiceReference1;
+
+public partial class asmx_test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        lyfePortsSoapClient c = new lyfePortsSoapClient();
+        Response.Write(c.HelloWorld());
+
+
+
+
+    }
+}
